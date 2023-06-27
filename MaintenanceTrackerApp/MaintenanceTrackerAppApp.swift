b274a1942adbde872d98c549099c24e2346c3639
Mark <menm619@gmail.com>
2023-06-26 23:34:17 -0700

    Initial Commit

---
+++ MaintenanceTrackerApp/MaintenanceTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaintenanceTrackerAppApp.swift
+//  MaintenanceTrackerApp
+//
+//  Created by Mark Martin on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MaintenanceTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
